31f08584a76f0a21a187967044779746ac03ec5c
Neil Albrock <neil@atomised.coop>
2013-09-18 21:24:16 +0100

    Search logs and done

--- hippybot/plugins/logentries.py
+++ hippybot/plugins/logentries.py
@@ -1,6 +1,8 @@
 # coding: utf-8
 import os
-import re
+import shlex
+import urllib
+
 import requests
 
 from hippybot.decorators import botcmd
@@ -10,26 +12,72 @@ class Plugin(object):
     """Plugin to access log data from Logentries
     """
     def __init__(self):
-        """Initialise plugin and load config
+        """Initialise plugin
 
         The following environment variables are used to hold configuration
 
+            LE_API_URL: Override the default API URL
             LE_API_KEY: Account key found on your account screen
             LE_LOGS: List of logs you want to access
                      i.e. "key:host/log,key:host/log"
+            LE_GEN_URL: Boolean -
+                        Return a full URL for the search (reveals API Key)
 
         """
-        self.le_conf = {}
-        self.le_conf['api_key'] = os.environ.get('LE_API_KEY','')
+        self.le_api_url = os.environ.get('LE_API_URL','https://api.logentries.com/')
+        self.le_api_key = os.environ.get('LE_API_KEY','')
         lgs = os.environ.get('LE_LOGS','')
         lgs = { k:v for (k,v) in [ l.split(':') for l in lgs.split(',') ] }
-        self.le_conf['logs'] = lgs
+        self.le_logs = lgs
+        self.le_gen_url = os.environ.get('LE_GEN_URL', 'false') \
+                                    .lower() in ('true','t','yes','1')
 
     @botcmd(name='le-list')
     def list_logs(self, mess, args):
-        """
+        """List available logs
+
         Returns a list of available logs to search
+
         Format: @NickName le-list
+
         """
         self.bot.log.info("le-list: %s" % mess)
-        return u", ".join(self.le_conf['logs'].keys())
+        logs = u", ".join(self.le_logs.keys())
+
+        if logs:
+            return logs
+        else:
+            return u"There are no logs defined..."
+
+    @botcmd(name='le-search')
+    def search_logs(self, mess, args):
+        """Search logs
+
+        Returns logs entries matching query
+        The log key can be found by using the le-list command
+
+        Format: @NickName le-search <log> <query>
+
+        """
+        self.bot.log.info("le-search: %s" % mess)
+
+        log, query = shlex.split(args.encode('utf8'))
+        url = self.gen_url(log, query)
+        req = requests.get(url)
+        reply = req.content
+
+        if self.le_gen_url:
+            reply = "{}\n{}".format(reply, url)
+
+        return reply
+
+    def gen_url(self, log, query):
+        """Generate search URL
+
+        Compose a valid search URL to query the API
+
+        """
+        params = (('filter',query),('limit',5))
+        url = "{}{}/hosts/{}/?{}".format(self.le_api_url, self.le_api_key,
+                                        self.le_logs[log], urllib.urlencode(params))
+        return url
